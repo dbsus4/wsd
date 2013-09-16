e07277e08f0ee07ba811e2602c58fe2a9a90dd94
dbsus4 <dbsus4@gmail.com>
2013-09-16 19:24:44 +0200

    gpio test file modified

--- gpiotest.py
+++ gpiotest.py
@@ -1,37 +1,48 @@
 #!/usr/bin/python
 
-import RPi.GPIO as GPIO
+##import RPi.GPIO as GPIO
 import time
 
 # Open SPI device
 dev 		= "/dev/spidev0.0"
-spidev 		= file(dev, "wb")
+##spidev 		= file(dev, "wb")
 prevTime 	= 0.0
 sel 			= True
 
-pixelA		= [bytearray(3) for i in range(5)]
-for i in range(len(pixelA)):
-	if i%2:
-		pixelA[i][0] = 255
-		pixelA[i][1] = 0
-		pixelA[i][2] = 0
+L = 5
 
-pixelB		= [bytearray(3) for i in range(5)]
-for i in range(len(pixelB)):
-	if not (i%2):
-		pixelB[i][0] = 255
-		pixelB[i][1] = 0
-		pixelB[i][2] = 0
+pixelA		= bytearray(L*3)
+for h in range(L):
+	i = h*3
+	if h%2:
+		pixelA[i+0] = int(255)
+	else:
+		pixelA[i+0] = int(0)
+
+	pixelA[i+1] = int(0)
+	pixelA[i+2] = int(0)
+
+pixelB		= bytearray(L*3)
+for h in range(L):
+	i = h*3
+	pixelB[i+0] = int(0)
+
+	if h%2:
+		pixelB[i+1] = int(255)
+	else:
+		pixelB[i+1] = int(0)
+
+	pixelB[i+2] = int(0)
 
 def display():
 	if(sel):
 		print pixelA
-		spidev.write(pixelA)
+		##spidev.write(pixelA)
 	else:
 		print pixelB
-		spidev.write(pixelB)
+		##spidev.write(pixelB)
 
-	spidev.flush()
+	##spidev.flush()
 	time.sleep(0.001)
 
 # main
